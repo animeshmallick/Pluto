899f59e77fab14006cbee120b2b18ee5a9c2accb
= <animesh7995@gmail.com>
2024-09-15 20:03:29 +0530

    Logut feature implementation

--- src/main/java/org/example/pluto/servlets/Logout.java
+++ src/main/java/org/example/pluto/servlets/Logout.java
@@ -1,23 +1,34 @@
 package org.example.pluto.servlets;
 
-import javax.servlet.ServletConfig;
-import javax.servlet.ServletException;
+import com.google.gson.Gson;
+import org.json.simple.JSONObject;
+
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @WebServlet(name = "Logout", value = "/Logout")
 public class Logout extends HttpServlet {
-    public Logout() {
-        System.out.println("init");
-    }
-    public void init(ServletConfig config) throws ServletException {
-        System.out.println("init");
-    }
-    protected void doGet(HttpServletRequest request, HttpServletResponse response){
-        System.out.println("doGet1  ");
-        destroy();
-        System.out.println("doGet");
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+
+        boolean logged_out = false;
+        JSONObject obj = new JSONObject();
+        for (Cookie cookie : request.getCookies()) {
+            if (cookie.getName().equals("auth_user_id")) {
+                cookie.setMaxAge(0);
+                response.addCookie(cookie);
+                obj.put("success", "Successfully logged out");
+                logged_out = true;
+            }
+        }
+        if (!logged_out) {
+            obj.put("error", "Nothing to logout from");
+        }
+        response.getWriter().println((new Gson()).toJson(obj));
     }
 }
